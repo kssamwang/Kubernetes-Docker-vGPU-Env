9e385eb0505e23e88520b877b6e60960562b1142
Shangyou Wang <51863110+kssamwang@users.noreply.github.com>
2023-05-19 14:45:44 +0800

    Update install.sh

--- 4-go-install/install.sh
+++ 4-go-install/install.sh
@@ -1,5 +1,7 @@
-wget https://dl.google.com/go/go1.20.linux-amd64.tar.gz
+#!/bin/bash
+wget https://mirrors.nju.edu.cn/golang/go1.20.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
 sudo echo -e "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 source /etc/profile
+rm -f go1.20.linux-amd64.tar.gz
 go version
