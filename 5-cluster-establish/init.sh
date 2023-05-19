87850c2ceded036be3fc028f8f45f90d2988f806
Shangyou Wang <51863110+kssamwang@users.noreply.github.com>
2023-05-19 20:00:08 +0800

    Delete init.sh

--- 5-cluster-establish/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-masterip=$(ifconfig eth0 | egrep "inet [0-9]+.[0-9]+.[0-9]+.[0-9]+" | awk '{ print $2 }')
-echo "k8s cluster init .... master host ip $masterip"
-kubeadm init --apiserver-advertise-address=$masterip --apiserver-cert-extra-sans=127.0.0.1 --pod-network-cidr=10.244.0.0/16 --image-repository=registry.aliyuncs.com/google_containers
