17590c1d36db064d2bc397a329577ef0f2aabf7c
Shangyou Wang <51863110+kssamwang@users.noreply.github.com>
2023-05-19 14:02:46 +0800

    Delete update.admin.conf.sh

--- 9-cgroupfs/update.admin.conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cnt=$(cat ~/.bash_profile | grep "export KUBECONFIG=/etc/kubernetes/admin.conf" | wc -l)
-if [ $cnt -eq 0 ];then
-	echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> ~/.bash_profile	
-fi
-source ~/.bash_profile
-
