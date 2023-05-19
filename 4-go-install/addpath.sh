cc69b896acbf4e8c45e40dbe0de924895b660896
Shangyou Wang <51863110+kssamwang@users.noreply.github.com>
2023-05-19 14:48:29 +0800

    Update addpath.sh

--- 4-go-install/addpath.sh
+++ 4-go-install/addpath.sh
@@ -1,3 +1,7 @@
-sudo echo -e "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+#!/bin/bash
+cnt=$(cat /etc/profile | grep "export PATH=$PATH:/usr/local/go/bin" | wc -l)
+if [ $cnt -eq 0 ];then
+	echo -e "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+fi
 source /etc/profile
 go version
